b4106b05a9b4ee36ab5dbd0c32703c012e17db98
raviNavarro1226 <ravi.navarro@madeinweb.app>
2022-10-06 10:40:45 -0300

    changes on border bar chart

--- BarChartRenderer.swift
+++ BarChartRenderer.swift
@@ -379,7 +379,12 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
                 context.setFillColor(dataSet.color(atIndex: j).cgColor)
             }
             
-            context.fill(barRect)
+            let bezierPath = UIBezierPath(roundedRect:barRect,
+                                          byRoundingCorners:[.allCorners],
+                                          cornerRadii: CGSize(width: barRect.width / 2, height:  barRect.width / 2))
+            
+            context.addPath(bezierPath.cgPath)
+            context.drawPath(using: .fill)
             
             if drawBorder
             {
@@ -744,7 +749,12 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
                 
                 setHighlightDrawPos(highlight: high, barRect: barRect)
                 
-                context.fill(barRect)
+                let bezierPath = UIBezierPath(roundedRect:barRect,
+                                              byRoundingCorners:[.allCorners],
+                                              cornerRadii: CGSize(width: barRect.width / 2, height:  barRect.width / 2))
+                
+                context.addPath(bezierPath.cgPath)
+                context.drawPath(using: .fill)
             }
         }
     }
